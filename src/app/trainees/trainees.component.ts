30da79ea5d9d16f1e2d49abdac69ec35bd8ceb42
yangqian <743295483@qq.com>
2020-10-29 19:57:08 +0800

    [yangqian]: init: groups trainee trainer component.

---
+++ src/app/trainees/trainees.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trainees',
+  templateUrl: './trainees.component.html',
+  styleUrls: ['./trainees.component.scss']
+})
+export class TraineesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
